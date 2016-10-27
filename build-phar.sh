8a159c7ccb6109806def7cf29bd7c088dcab227f
Tom Valk <tomvalk@gmail.com>
2016-10-27 16:35:34 +0200

    [TASK] Add build script and dependencies to magento marketplace-eqp.

---
+++ build-phar.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# To prevent error messages from composer, make empty dirs.
+mkdir -p CodeSniffer/Standards/MEQP1/Sniffs
+mkdir -p CodeSniffer/Standards/MEQP2/Sniffs
+
+# Update dependencies.
+composer update
+
+# Copy magento sniffs.
+cp -R vendor/magento/marketplace-eqp/AbstractSniffs CodeSniffer/Standards/
+cp -R vendor/magento/marketplace-eqp/Utils CodeSniffer/Standards/
+cp -R vendor/magento/marketplace-eqp/MEQP1 CodeSniffer/Standards/
+cp -R vendor/magento/marketplace-eqp/MEQP2 CodeSniffer/Standards/
+
+# Build phar.
+php vendor/bin/phar-composer build phpcs.phar
